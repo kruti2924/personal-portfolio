fc76a478f97d6e931e3a439d7391b2e18ed7881c
kruti2924 <krutidharsandiya2924@gmail.com>
2025-08-23 02:16:37 +0530

    package.json-update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/personal-portfolio/",
+  // base: "/personal-portfolio/",
+  base: './',
   plugins: [react()],
 });
